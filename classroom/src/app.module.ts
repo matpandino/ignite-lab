7be51dce8009848feea25f0363045b0ab57a39c2
Mateus Pandino <mpandino@avantstay.com>
2022-05-17 01:37:15 -0300

    Add microservices do apis

--- classroom/src/app.module.ts
+++ classroom/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { HttpModule } from './http/http.module';
+import { PurchaseController } from './messaging/controllers/purchases.controller';
+import { MessagingModule } from './messaging/messaging.module';
 
 @Module({
-  imports: [DatabaseModule, HttpModule],
-  controllers: [],
+  imports: [DatabaseModule, HttpModule, MessagingModule],
+  controllers: [PurchaseController],
   providers: [],
 })
 export class AppModule {}
