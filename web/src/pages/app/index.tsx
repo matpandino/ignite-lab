111e0203cefa38013ce6093b4b2fc96f13a7a188
Mateus Pandino <matpandino@gmail.com>
2022-05-30 20:36:25 -0300

    add tailwindcss

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { withApollo } from "../../lib/withApollo";
-import { useGetProductsQuery } from "../../graphql/generated/graphql";
 import {
-  getServerPageGetProducts,
   ssrGetProducts,
   useMe,
 } from "../../graphql/generated/page";
@@ -19,7 +17,7 @@ export function Home({data}) {
       <pre>{JSON.stringify(data, null, 2)}</pre>
       <pre>{JSON.stringify(user, null, 2)}</pre>
 
-      <a href="/api/auth/logout">Logout</a>
+      {/* <a href="/api/auth/logout">Logout</a> */}
     </div>
   );
 }
